db45c9b26c9702cf03e301e764b25940d2ddb8e4
Scott Schwab <scottschwab86@gmail.com>
2024-03-18 10:22:46 -0500

    Complete challenge

--- javascript/tree_traversal_inorder.js
+++ javascript/tree_traversal_inorder.js
@@ -6,8 +6,27 @@ class Node {
   }
 }
 
-function treeTraversalInorder(root) {
-  // type your code here
+function treeTraversalInorder(root, result = []) {
+  // does root have left?
+    //  if yes, call treeTraversalInOrder with left (and result)
+    //  push node to result
+    // if node has right, call treeTraversalInOrder with right (and result)
+    // return result
+
+    if (!root) {
+      return [];
+    }
+
+    if (root.left) {
+      treeTraversalInorder(root.left, result);
+    }
+    result.push(root.value);
+
+    if (root.right) {
+      treeTraversalInorder(root.right, result);
+    }
+    return result;
+
 }
 
 if (require.main === module) {
@@ -20,6 +39,24 @@ if (require.main === module) {
 
   root = new Node(10, new Node(0, null, new Node(5)), new Node(20, null, new Node(30)));
   console.log("Expecting: [0, 5, 10, 20, 30] ");
+  console.log(treeTraversalInorder(root));
+
+  console.log("");
+
+  root = new Node(10, new Node(5, new Node(3), new Node(6)), new Node(12, null, new Node(16)))
+  console.log("Expecting: [3, 5, 6, 10, 12, 16] ");
+  console.log(treeTraversalInorder(root));
+
+  console.log("");
+
+  root = new Node(5)
+  console.log("Expecting: [5] ");
+  console.log(treeTraversalInorder(root));
+
+  console.log("");
+
+  root = new Node()
+  console.log("Expecting: [] ");
   console.log(treeTraversalInorder(root));
 }
 
